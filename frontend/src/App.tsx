1257c29d6a21acc50445d65444bc5c2f7487f854
tabrez05 <ahamedsmd007@gmail.com>
2025-04-16 01:22:01 -0700

    chore: update routing and dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Topics from './pages/Topics';
-import Tutorial from './pages/Tutorial';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import TutorialPage from './pages/TutorialPage';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -14,13 +14,13 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
-        <main className="container mx-auto px-4 py-8">
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/topics" element={<Topics />} />
-            <Route path="/tutorial/:id" element={<Tutorial />} />
             <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
             <Route path="/register" element={<Register setIsAuthenticated={setIsAuthenticated} />} />
+            <Route path="/tutorial/:id" element={<TutorialPage />} />
           </Routes>
         </main>
       </div>
